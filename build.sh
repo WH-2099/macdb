23910a0b9442e908dc2421f83eccfa8ff54fba80
WH-2099 <wh2099@outlook.com>
2021-12-11 14:38:05 +0800

    增加对 sqlite 的校验

--- build.sh
+++ build.sh
@@ -15,21 +15,22 @@ merge_csv() {
 
 }
 
-commit() {
-    git config user.name 'WH-2099 CI/CD'
-    git config user.email 'github-actions@github.com'
-    git add sha256sum.txt mac.csv mac.db
-    git commit -m "CI/CD Auto Update"
-    git push
+check_to_commit() {
+    if ! sha256sum -c sha256sum.txt; then
+        sha256sum mac.csv mac.db >sha256sum.txt
+        git config user.name 'WH-2099 CI/CD'
+        git config user.email 'github-actions@github.com'
+        git add sha256sum.txt mac.csv mac.db
+        git commit -m "CI/CD Auto Update"
+        git push
+    fi
 }
 
-build() {
-    if ! sha256sum -c sha256sum.txt; then
-        sha256sum mac.csv >sha256sum.txt
-        dbfile='mac.db'
-        table='mac'
-        rm "$dbfile"
-        sqlite3 "$dbfile" <<_EOF
+build_sqlite() {
+    dbfile='mac.db'
+    table='mac'
+    rm "$dbfile"
+    sqlite3 "$dbfile" <<_EOF
         CREATE TABLE $table (
             registry TEXT,
             assignment TEXT COLLATE NOCASE,
@@ -38,14 +39,13 @@ build() {
         );
         CREATE INDEX assignment_index ON $table (assignment);
 _EOF
-        sqlite3 "$dbfile" ".import --csv --skip 1 'mac.csv' $table"
-        commit
-    fi
+    sqlite3 "$dbfile" ".import --csv --skip 1 'mac.csv' $table"
 }
 
 main() {
     download
     merge_csv
-    build
+    build_sqlite
+    check_to_commit
 }
 main
